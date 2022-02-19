15775a9874af48b93bf939e92cc6c12860bf0c22
Erika Yamamoto <erika.s.yamamoto@gmail.com>
2022-02-18 23:26:40 -0300

    Implementando TEMA no blog pessoal

---
+++ src/app/bem-vinde/bem-vinde.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bem-vinde',
+  templateUrl: './bem-vinde.component.html',
+  styleUrls: ['./bem-vinde.component.css']
+})
+export class BemVindeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
