58e2f3612804e3e54aa9c53ba3b299f3563fc193
Erika Yamamoto <erika.s.yamamoto@gmail.com>
2022-03-02 17:06:33 -0300

    PUT E DELETE temas

--- src/app/delete/tema-delete/tema-delete.component.ts
+++ src/app/delete/tema-delete/tema-delete.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Tema } from 'src/app/model/Tema';
+import { TemaService } from 'src/app/service/tema.service';
+import { environment } from 'src/environments/environment.prod';
 
 @Component({
   selector: 'app-tema-delete',
@@ -7,9 +11,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TemaDeleteComponent implements OnInit {
 
-  constructor() { }
+  tema: Tema = new Tema()
+  id: number
 
-  ngOnInit(): void {
+  constructor(
+
+    private temaService: TemaService,
+    private router: Router,
+    private route: ActivatedRoute
+
+  ) { }
+
+  ngOnInit(){
+    if (environment.token == '') {
+      this.router.navigate(['/entrar'])
   }
+
+ this.id = this.route.snapshot.params['idTema']
+  this.findByIdTema(this.id)
+}
+
+findByIdTema(id: number) {
+  this.temaService.getByIdTema(id).subscribe((resp: Tema) => {
+    this.tema = resp
+  })
+}
+
+apagar() {
+  this.temaService.deleteTema(this.id).subscribe(() => {
+    alert('Tema apagado com sucesso!!')
+    this.router.navigate(['/tema'])
+  })
+}
 
 }
--- src/app/edit/tema-edit/tema-edit.component.ts
+++ src/app/edit/tema-edit/tema-edit.component.ts
@@ -33,4 +33,13 @@ findByidTema(idTema: number){
     this.tema = resp
   })
 }
+
+atualizar(){
+  this.temaService.putTema(this.tema).subscribe((resp: Tema)=>{
+    this.tema = resp
+    alert('Tema atualizado com sucesso!!')
+    this.router.navigate(['/tema'])
+  })
+}
+
 }
